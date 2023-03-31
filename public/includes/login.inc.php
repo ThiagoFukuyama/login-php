70ad695f39e5f1c1c5648ccfeb45f1eaa0a21852
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-03-31 15:02:57 -0300

    Removidos comentários redundantes

--- public/includes/login.inc.php
+++ public/includes/login.inc.php
@@ -6,20 +6,13 @@ use Source\Controllers\LoginController;
 
 if (isset($_POST["submit"])) {
 
-    // Pegando os dados
     $uid = $_POST["uid"];
     $password = $_POST["password"];
 
-
-    // Instanciando classe LoginController
     $login  = new LoginController($uid, $password); 
 
-
-    // Rodando registro de usuário
     $login->loginUser();
 
-    
-    // Voltando para a página inicial
     header("Location: ../loggedInPage.php");
     exit();
 
--- public/includes/signup.inc.php
+++ public/includes/signup.inc.php
@@ -6,22 +6,15 @@ use Source\Controllers\SignupController;
 
 if (isset($_POST["submit"])) {
 
-    // Pegando os dados
     $uid = $_POST["uid"];
     $password = $_POST["password"];
     $passwordRepeat = $_POST["passwordRepeat"];
     $email = $_POST["email"];
 
-
-    // Instanciando classe SignupContr
     $signup  = new SignupController($uid, $password, $passwordRepeat, $email); 
 
-
-    // Rodando registro de usuário
     $signup->signupUser();
 
-    
-    // Voltando para a página inicial
     session_start();
     $_SESSION["signupError"] = "none";
     header("Location: ../");
